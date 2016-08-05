1fa0c163389dd95c53d81c313c36beb437bb591f
M. Blank <max.blank@googlemail.com>
2016-08-05 22:32:46 +0200

    - GUI, first try

--- gradeChecker.py
+++ gradeChecker.py
@@ -10,80 +10,144 @@
 # Please use with care! I'm not to be held responsible for any damage
 # this software might cause to anyone or anything!
 
-import sys
-import re
+
 import webbrowser
 import os
+from threading import *
+from Tkinter import *
 from mechanize import Browser
 from urllib2 import URLError
 from time import sleep
 
+Version = "v0.3"
 
-nachname = ""   # last name of student
-vorname = ""    # first name(s) of student
-gebdatum = ""   # birthday of student
-gebort = "" # birthplace of student
-mcard = ""  # badge number of student
-gradepath = "noten.html"    # path to store grade html-file
+# Nachname = "Blank"   # last name of student
+# Vorname = "Maximilian Alexander"    # first name(s) of student
+# GebDatum = "20.02.1989"   # birthday of student
+# GebOrt = "München" # birthplace of student
+# MCard = "88948888012"  # badge number of student
+GradePath = "noten.html"    # path to store grade html-file
 
-if len(sys.argv) == 1:
-    print "Not enough parameters!"
 
-browser = Browser()
+def checkgrades():
 
-while True:     # trying to load primuss.de
-    try:
-        response = browser.open("https://www1.primuss.de/cgi/Sesam/sesam.pl?FH=fhm")
-        break
+    # get values from entry fields
+    nachname = enterNachname.get()
+    vorname = enterVorname.get()
+    gebdatum = enterGebDatum.get()
+    gebort = enterGebOrt.get().encode('iso-8859-1')
+    mcard = enterMCard.get()
+    print gebort
 
-    except URLError:
-        print "Timeout requesting login form; trying again!"
-        sleep(20)
-        pass
+    browser = Browser()
 
-# Fill login form
-print "Logging in..."
-browser.select_form('AnmeldeForm')
-browser.form['Nachname'] = nachname
-browser.form['Vorname'] = vorname
-browser.form['GebDatum'] = gebdatum
-browser.form['GebOrt'] = gebort
-browser.form['MCard'] = mcard
-browser.submit()
-
-# if login failed:
-if (re.search('<span class="Fehler">Fehler:</span> Anmeldung fehlerhaft, kein Zugriff!</p>', browser.response().read())
-        is not None):
-    print "Login failed! Check credentials!"
-else:
-    print "Logged in!"
-    print "Searching grades..."
-    browser.follow_link(text="Services")    # navigate to grade view
-    for form in browser.forms():
-        if form.attrs["action"]=="https://www3.primuss.de/cgi-bin/pg_Notenbekanntgabe/index.pl":
-            browser.form = form
-            break
-
-    browser.submit()
-    browser.select_form('Form')
-    notenurl = "https://www3.primuss.de/cgi-bin/pg_Notenbekanntgabe/showajax.pl?Language=de&Session="
-    sessionid = browser.form['Session']
-    poison = browser.form['Poison']
-    while True:
+    while True:     # trying to load primuss.de
         try:
-            browser.open(notenurl + sessionid + "&Poison=" + poison + "&User=" + mcard + "&FH=fhm&Accept=X")
+            browser.open("https://www1.primuss.de/cgi/Sesam/sesam.pl?FH=fhm")
             break
 
         except URLError:
-            print "Timeout openening grade view! Trying again..."
-            sleep(10)
+            print "Timeout requesting login form; trying again!"
+            sleep(20)
             pass
 
-    print "Found grades!"
-    print "Saving grades..."
-    gradefile = open(gradepath, "w+")
-    gradefile.write(browser.response().read())
-    gradefile.close()
-    print "Grades saved!"
-    print "Displaying grades..."
-    webbrowser.open('file://' + os.path.realpath(gradepath))
+    # Fill login form
+    print "Logging in..."
+    browser.select_form('AnmeldeForm')
+    browser.form['Nachname'] = nachname
+    browser.form['Vorname'] = vorname
+    browser.form['GebDatum'] = gebdatum
+    browser.form['GebOrt'] = gebort
+    browser.form['MCard'] = mcard
+    browser.submit()
+
+    # if login failed:
+    if (re.search('<span class="Fehler">Fehler:</span> Anmeldung fehlerhaft, kein Zugriff!</p>', browser.response().read())
+            is not None):
+        print "Login failed! Check credentials!"
+    else:
+        print "Logged in!"
+        print "Searching grades..."
+        browser.follow_link(text="Services")    # navigate to grade view
+        for form in browser.forms():
+            if form.attrs["action"]=="https://www3.primuss.de/cgi-bin/pg_Notenbekanntgabe/index.pl":
+                browser.form = form
+                break
+
+        browser.submit()
+        browser.select_form('Form')
+        notenurl = "https://www3.primuss.de/cgi-bin/pg_Notenbekanntgabe/showajax.pl?Language=de&Session="
+        sessionid = browser.form['Session']
+        poison = browser.form['Poison']
+        while True:
+            try:
+                browser.open(notenurl + sessionid + "&Poison=" + poison + "&User=" + mcard + "&FH=fhm&Accept=X")
+                break
+
+            except URLError:
+                print "Timeout opening grade view! Trying again..."
+                sleep(10)
+                pass
+
+        print "Found grades!"
+        print "Saving grades..."
+        gradefile = open(GradePath, "w+")
+        gradefile.write(browser.response().read())
+        gradefile.close()
+        print "Grades saved!"
+        print "Displaying grades..."
+        webbrowser.open('file://' + os.path.realpath(GradePath))
+
+
+# build gui
+root = Tk()
+root.title("gradeChecker " + Version)
+
+lab = Label(root, text="Das leidige Abfragen von Primuss automatisieren")
+lab.pack()
+
+labelNachname = Label(root, text="Nachname:")
+labelNachname.pack()
+enterNachname = Entry(root)
+enterNachname.pack()
+
+labelVorname = Label(root, text="Vorname(n):")
+labelVorname.pack()
+enterVorname = Entry(root)
+enterVorname.pack()
+
+labelGebDatum = Label(root, text="Geburtsdatum:")
+labelGebDatum.pack()
+enterGebDatum = Entry(root)
+enterGebDatum.pack()
+
+labelGebOrt = Label(root, text="Geburtsort:")
+labelGebOrt.pack()
+enterGebOrt = Entry(root)
+enterGebOrt.pack()
+
+labelMCard = Label(root, text="Magnetkarten-Nr.:")
+labelMCard.pack()
+enterMCard = Entry(root)
+enterMCard.pack()
+
+runningthread = Thread(target=checkgrades)
+
+def abort():
+    print "Abbrechen"
+    if runningthread.isAlive():
+        runningthread.join()
+
+def start():
+    print "Starting query..."
+    runningthread.start()
+
+buttonAbort = Button(root, text="Abbrechen", command=abort)
+buttonCheckGrades = Button(root, text="Abfrage", command=checkgrades)
+buttonCheckGrades.pack()
+buttonAbort.pack()
+
+root.mainloop()
+
+
+
